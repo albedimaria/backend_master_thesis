4872fd46b7b45633fc76ecaaeea9eace2926fab5
Alberto <alberto.dimaria@mail.polimi.it>
2023-10-11 01:47:45 +0200

    color update

--- frontend-thesis/client/src/contexts/levaControls/colorDropbox/ColorToHex.js
+++ frontend-thesis/client/src/contexts/levaControls/colorDropbox/ColorToHex.js
@@ -9,27 +9,27 @@ export function colorToHex(colorName) {
         case "yellow":
             return "#ffff00";
         case "yellowgreen":
-            return "#9ACD32";
+            return "#af0";
         case "limegreen":
-            return "#32CD32";
+            return "#0f0";
         case "green":
             return "#008000";
         case "seagreen":
-            return "#2E8B57";
+            return "#00ff6f";
         case "aquamarine":
-            return "#26fab0";
+            return "#00ffb7";
         case "lightblue":
-            return "#00c3fa";
+            return "#00c8ff";
         case "steelblue":
-            return "#01598c";
+            return "#0091ff";
         case "blue":
             return "#0000FF";
         case "blueviolet":
-            return "#8A2BE2";
+            return "#5e00ff";
         case "violet":
-            return "#EE82EE";
+            return "#d0f";
         case "hotpink":
-            return "#FF69B4";
+            return "#ff00c8";
         case "red":
             return "#FF0000";
         case "white":
